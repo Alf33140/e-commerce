4f082fe354fa0793ace554105099891c53111f16
Alf33140 <alf2kdavy@yahoo.com>
2026-02-26 14:26:47 +0100

    mise en place securite admin

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class CategorieController extends AbstractController
 {
-    #[Route('/categoriecontroller', name: 'app_categorieController')]
+    #[Route('/admin/categoriecontroller', name: 'app_categorieController')] 
     public function index(CategorieRepository $repo,): Response
     {
             $categories = $repo->findAll(); // find all: renvoies moi toutes les categories de la base de données
@@ -22,7 +22,7 @@ final class CategorieController extends AbstractController
                 'categories' => $categories, // categories: c'est le nom de la variable que j'utilise dans mon template pour afficher les categories
         ]);
     }
-    #[Route('/categorie/new', name: 'app_categorie_new')]
+    #[Route('/admin/categorie/new', name: 'app_categorie_new')]
     public function addcategory(EntityManagerInterface $entityManager, Request $request): Response
     {
         $category = new Categorie();
@@ -34,6 +34,8 @@ final class CategorieController extends AbstractController
             $entityManager->persist($category);
            
             $entityManager->flush();
+            $this->addFlash('info','La catégorie a été ajoutée avec succès'); 
+           
             return $this->redirectToRoute('app_categorieController');
            
         }
@@ -42,7 +44,7 @@ final class CategorieController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
-    #[Route('/categorie/update/{id}', name:'app_categorie_update')]
+    #[Route('/admin/categorie/update/{id}', name:'app_categorie_update')]
     public function modifycategory(EntityManagerInterface $entityManager, Request $request, Categorie $category): Response
     { 
     
@@ -51,19 +53,19 @@ final class CategorieController extends AbstractController
         
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_home_page');
+            $this->addFlash('success','La modification a été réaalisé avec succès');
+            return $this->redirectToRoute('app_categorieController');
         }
         return $this->render('categoriecontroller/modifyCategorie.html.twig', [
             'form' => $form->createView(),
         ]);
     }
-    #[Route('/categoriecontroller/remove/{id}', name:'app_categorie_remove')]
+    #[Route('/admin/categoriecontroller/remove/{id}', name:'app_categorie_remove')]
     public function removecategory(EntityManagerInterface $entityManager, Categorie $category): Response
     {
         $entityManager->remove($category);
         $entityManager->flush();
-
-        return $this->redirectToRoute('app_categorie');
+        $this->addFlash('danger','La suppression a été réalisé avec succès');
+        return $this->redirectToRoute('app_categorieController');
     }
 }
