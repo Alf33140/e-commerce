17eea5f03dc26b6f025cad4328291040c5388530
Alf33140 <alf2kdavy@yahoo.com>
2026-02-26 12:05:30 +0100

    crud complet realisé

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Categorie;
 use App\Form\CatergoryFormType;
+use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Dom\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +14,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CategorieController extends AbstractController
 {
     #[Route('/categoriecontroller', name: 'app_categorieController')]
-    public function index(): Response
+    public function index(CategorieRepository $repo,): Response
     {
+            $categories = $repo->findAll(); // find all: renvoies moi toutes les categories de la base de données
+            
             return $this->render('categorieController/index.html.twig', [
-            'controller_name' => 'CategorieController',
+                'categories' => $categories, // categories: c'est le nom de la variable que j'utilise dans mon template pour afficher les categories
         ]);
     }
     #[Route('/categorie/new', name: 'app_categorie_new')]
@@ -32,11 +34,36 @@ final class CategorieController extends AbstractController
             $entityManager->persist($category);
            
             $entityManager->flush();
+            return $this->redirectToRoute('app_categorieController');
            
         }
         
         return $this->render('categoriecontroller/newCategorie.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+    #[Route('/categorie/update/{id}', name:'app_categorie_update')]
+    public function modifycategory(EntityManagerInterface $entityManager, Request $request, Categorie $category): Response
+    { 
+    
+        $form = $this->createForm(CatergoryFormType::class, $category);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home_page');
+        }
+        return $this->render('categoriecontroller/modifyCategorie.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/categoriecontroller/remove/{id}', name:'app_categorie_remove')]
+    public function removecategory(EntityManagerInterface $entityManager, Categorie $category): Response
+    {
+        $entityManager->remove($category);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_categorie');
     }
 }
