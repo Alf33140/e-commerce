29830f18cd6959139e667100e087be5045ee9d19
Alf33140 <alf2kdavy@yahoo.com>
2026-03-04 16:44:33 +0100

    form

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -10,16 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomePageController extends AbstractController
 {
-    #[Route('/', name: 'app_home_page')]
-    public function index(ProductRepository $productRepository): Response
+    #[Route('/', name: 'app_home_page')]  #cette annotation indique que la méthode index() est associée à la route racine ("/") de l'application. Lorsque les utilisateurs accèdent à cette URL, la méthode index() sera exécutée pour générer la réponse appropriée.
+    public function index(ProductRepository $productRepository): Response  #
     {
-        $products = $productRepository->findAll();
+        $products = $productRepository->findAll();   #Cette ligne utilise le ProductRepository pour récupérer tous les produits de la base de données. La méthode findAll() renvoie une liste de tous les produits disponibles, qui est ensuite stockée dans la variable $products.
 
     
         return $this->render('home_page/index.html.twig', [
-            'products' => $products,
+            'products' => $products,  #Cette ligne passe la variable $products à la vue Twig (home_page/index.html.twig) en tant que variable nommée 'products'. Cela permet à la vue d'accéder à la liste des produits et de les afficher de manière appropriée dans le template HTML.
             
-            
+      
         ]);
         
     }
