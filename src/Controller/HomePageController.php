5f9c0e068c71c455a0f2aaa6fcd55c67004492f5
Alf33140 <alf2kdavy@yahoo.com>
2026-03-15 22:28:28 +0100

    realisation du menu pour les user

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -14,12 +14,12 @@ final class HomePageController extends AbstractController
     #[Route('/', name: 'app_home_page')]  //cette annotation indique que la méthode index() est associée à la route racine ("/") de l'application. Lorsque les utilisateurs accèdent à cette URL, la méthode index() sera exécutée pour générer la réponse appropriée.
     public function index(ProductRepository $productRepository, CategorieRepository $categorieRepository): Response 
     {
-        $products = $productRepository->findAll();   
+        
 
     
         return $this->render('home_page/index.html.twig', [
-            'products' => $products,  //Cette ligne passe la variable $products à la vue Twig (home_page/index.html.twig) en tant que variable nommée 'products'. Cela permet à la vue d'accéder à la liste des produits et de les afficher de manière appropriée dans le template HTML.
-            'categorie' => $categorieRepository->findAll()
+            'products' => $productRepository->findAll(),
+            'categories' => $categorieRepository->findAll()
       
         ]);
         
